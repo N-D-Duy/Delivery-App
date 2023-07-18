a379c063b5a023682e34474b1945312bf3b68804
DuyNguyen <nguyenducduy160903@gmail.com>
2023-07-18 12:58:01 +0700

    xong cart repo

--- app/src/main/java/com/example/deliveryapp/repository/CartRepositoryImp.kt
+++ app/src/main/java/com/example/deliveryapp/repository/CartRepositoryImp.kt
@@ -3,6 +3,7 @@ package com.example.deliveryapp.repository
 import com.example.deliveryapp.model.Cart
 import com.example.deliveryapp.util.FirebaseCollections
 import com.example.deliveryapp.util.UiState
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
@@ -14,6 +15,7 @@ class CartRepositoryImp(val database: FirebaseFirestore): CartRepository{
     //
     override fun getCart(userId: String): Flow<Result<Cart?>> {
         return flow {
+            //tìm cart
             val cartSnapshot = database.collection(FirebaseCollections.CART)
                 .whereEqualTo("user_id", userId)
                 .get()
@@ -33,13 +35,48 @@ class CartRepositoryImp(val database: FirebaseFirestore): CartRepository{
 
     override fun addToCart(userId: String, foodId: String, quantity: Int): Flow<UiState<Unit>> {
         return flow {
-
+            //tìm cart
+            emit(UiState.Loading)
+            val cartSnapshot = database.collection(FirebaseCollections.CART)
+                .whereEqualTo("user_id", userId)
+                .get()
+                .await()
+            //tham chiếu food_list và thêm entry mới
+            if (!cartSnapshot.isEmpty) {
+                val cart = cartSnapshot.documents[0].toObject(Cart::class.java)
+                val newFood:MutableMap<String, Int> = mutableMapOf(
+                    foodId to quantity
+                )
+                val cartId = cart!!.cartId!!
+                database.collection(FirebaseCollections.CART).document(cartId).update("food_list", newFood)
+                emit(UiState.Success(Unit))
+            } else {
+                emit(UiState.Error("Cart not found"))
+            }
         }
     }
 
     override fun removeFromCart(userId: String, foodId: String): Flow<UiState<Unit>> {
         return flow {
-
+            //tìm cart
+            emit(UiState.Loading)
+            val cartSnapshot = database.collection(FirebaseCollections.CART)
+                .whereEqualTo("user_id", userId)
+                .get()
+                .await()
+            //tham chiếu food_list và xóa một entry (update lại food_list)
+            if (!cartSnapshot.isEmpty) {
+                val cart = cartSnapshot.documents[0].toObject(Cart::class.java)
+                val cartId = cart!!.cartId!!
+                val updateMap = mapOf<String, Any>(
+                    "food_list.$foodId" to FieldValue.delete()
+                )
+                database.collection(FirebaseCollections.CART).document(cartId)
+                    .update(updateMap)
+                emit(UiState.Success(Unit))
+            } else {
+                emit(UiState.Error("Cart not found"))
+            }
         }
     }
 
@@ -55,8 +92,25 @@ class CartRepositoryImp(val database: FirebaseFirestore): CartRepository{
 
     override fun clearCart(userId: String): Flow<UiState<Unit>> {
         return flow {
-
+            //tìm cart
+            emit(UiState.Loading)
+            val cartSnapshot = database.collection(FirebaseCollections.CART)
+                .whereEqualTo("user_id", userId)
+                .get()
+                .await()
+            //tham chiếu food_list và clear nó
+            if (!cartSnapshot.isEmpty) {
+                val cart = cartSnapshot.documents[0].toObject(Cart::class.java)
+                val cartId = cart!!.cartId!!
+                val updateMap = mapOf<String, Any>(
+                    "food_list" to emptyMap<String, Int>()
+                )
+                database.collection(FirebaseCollections.CART).document(cartId)
+                    .update(updateMap)
+                emit(UiState.Success(Unit))
+            } else {
+                emit(UiState.Error("Cart not found"))
+            }
         }
     }
-
 }
