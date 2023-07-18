d9483c17e8a6f50389fd77e7c56c93e634232b33
hoang son <hson83869999@gmail.com>
2023-07-19 00:02:51 +0700

    19/7

--- app/src/main/java/com/example/deliveryapp/adapter/WelcomeViewPagerAdapter.kt
+++ app/src/main/java/com/example/deliveryapp/adapter/WelcomeViewPagerAdapter.kt
@@ -3,12 +3,16 @@ package com.example.deliveryapp.adapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.deliveryapp.ui.activities.WelcomeActivity
 import com.example.deliveryapp.ui.fragments.SkipFragment1
 import com.example.deliveryapp.ui.fragments.SkipFragment2
 import com.example.deliveryapp.ui.fragments.SkipFragment3
 
-class ViewPagerAdapter(fragmentManager: androidx.fragment.app.FragmentManager, lifecycle: Lifecycle):
-    FragmentStateAdapter(fragmentManager,lifecycle) {
+class ViewPagerAdapter(
+    fragmentManager: WelcomeActivity,
+    lifecycle: Lifecycle
+) :
+    FragmentStateAdapter(fragmentManager) {
 
     override fun getItemCount(): Int {
         return 3
@@ -16,11 +20,9 @@ class ViewPagerAdapter(fragmentManager: androidx.fragment.app.FragmentManager, l
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> {SkipFragment1()}
-            1 -> {
-                SkipFragment2()
-            }
-            else -> {SkipFragment3()}
+            0 -> SkipFragment1()
+            1 -> SkipFragment2()
+            else -> SkipFragment3()
 
         }
     }
